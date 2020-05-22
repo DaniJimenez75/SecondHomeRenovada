e09c7160f158a1509445a752bead309cdaf71cde
Daniel Jiménez <djimenez000000@gmail.com>
2020-05-22 11:22:57 +0200

    Login usuarios

--- public/js/app.js
+++ public/js/app.js
@@ -1,118 +1,94 @@
 // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBFgJ_abbrHfju-0uE70fIgeDb4zSuZI40",
-    authDomain: "usuarios-bf872.firebaseapp.com",
-    databaseURL: "https://usuarios-bf872.firebaseio.com",
-    projectId: "usuarios-bf872",
-    storageBucket: "",
-    messagingSenderId: "296481844448",
-    appId: "1:296481844448:web:e1a463f1e90a161e"
+var firebaseConfig = {
+    apiKey: "AIzaSyDlcUbdttXusuSRWw42OnA2EPK5rPJVlTI",
+    authDomain: "second-home-studio-be3b8.firebaseapp.com",
+    databaseURL: "https://second-home-studio-be3b8.firebaseio.com",
+    projectId: "second-home-studio-be3b8",
+    storageBucket: "second-home-studio-be3b8.appspot.com",
+    messagingSenderId: "981645759516",
+    appId: "1:981645759516:web:782296eda069fe43483bd9"
   };
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  var proy=firebase.initializeApp(firebaseConfig);
 
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var textoVerificado = "";
-      if (emailVerified === false) {
-        textoVerificado = "Email no verificado";
-      }
-      else {
-        textoVerificado = "Email verificado";
-      }
-      var providerData = user.providerData;
-      document.getElementById('login').innerHTML =
-        `<p>Logueado ` + user.email + ` ` + textoVerificado + ` <p>
-  `;
-      document.getElementById('botonAcceso').style.display="none";
-      document.getElementById('checkLogin').style.display="none";
-      document.getElementById('areaRegistro').style.display="none";
-      document.getElementById('passA').style.display="none";
-      document.getElementById('areaLogin').style.display="";
-      document.getElementById('btnCerrar').style.display="";
-      document.getElementById('emailA').value=email;
-
-
-
-
-
-      console.log(user);
-    } else {
-      //document.getElementById('login').innerHTML = "No Logueado ";
-      document.getElementById('botonAcceso').style.display="";
-      document.getElementById('checkLogin').style.display="";
-      document.getElementById('areaRegistro').style.display="";
-      document.getElementById('passA').style.display="";
-      document.getElementById('areaLogin').style.display="none";
-      document.getElementById('btnCerrar').style.display="none";
-
-    }
-  });
-
-
-
-
-
-
-  function enviar() {
-    var email = document.getElementById('email').value;
-    var pass = document.getElementById('pass').value;
-    firebase.auth().createUserWithEmailAndPassword(email, pass)
-      .catch(function (error) {
+function enviar(){
+    var email=document.getElementById('email').value;
+      var pass=document.getElementById('pass').value;
+      //alert("email="+email+" pass="+pass);
+  
+      firebase.auth().createUserWithEmailAndPassword(email, pass)
+      .catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
         alert(errorMessage);
-      })
-      .then(function () {
-        verificar();
-      });
+  })
+  .then(function(){
+    verificar();
+  });
   }
 
-  function verificar() {
+  function verificar(){
     var user = firebase.auth().currentUser;
-    user.sendEmailVerification().then(function () {
+    user.sendEmailVerification().then(function() {
       // Email sent.
-    }).catch(function (error) {
+    }).catch(function(error) {
       // An error happened.
     });
+
   }
-  function acceso() {
-    var emailA = document.getElementById('emailA').value;
-    var passA = document.getElementById('passA').value;
+
+  function acceso(){
+    var emailA=document.getElementById('emailA').value;
+    var passA=document.getElementById('passA').value;
+    emailA.innerHTML="";
+    passA.innerHTML="";
+
     firebase.auth().signInWithEmailAndPassword(emailA, passA)
-      .catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-      });
-  }
-
-  function cerrar() {
-    firebase.auth().signOut()
-      .then(function () {
-        console.log('Salir');
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-
-  $(document).ready(function(){
-    $('#loginRestro').change(function(){
-      if($(this).is(':checked')){
-        $('#areaLogin').hide();
-        $('#areaRegistro').show();
-      }
-      else{
-        $('#areaLogin').show();
-        $('#areaRegistro').hide();
-      }
+    .catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        
     });
-  });
+}
+function observador(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log("Existe usuario activo");
+
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          if(emailVerified == false){
+            sessionStorage.setItem("emailVerified", emailVerified);
+
+          }
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+          sessionStorage.setItem("email", email);
+          location.href ="inicio.html";
+        } else {
+          // User is signed out.
+          // ...
+          console.log("No existe usuario activo");
+        }
+      });
+}
+
+function cerrar(){
+    firebase.auth().signOut()
+    .then(function(){
+        console.log("Salienddo...");
+        location.href ="login.html";
+
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
